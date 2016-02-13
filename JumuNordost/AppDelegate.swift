46ccfdbc28c3585594cd440c69597db61767dc5f
Martin Richter <me@martinrichter.net>
2016-02-13 14:36:13 +0100

    Create empty view controller for contest selection

--- JumuNordost/AppDelegate.swift
+++ JumuNordost/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 
         window = UIWindow()
-        window?.rootViewController = UIViewController()
+        window?.rootViewController = ContestsViewController()
         window?.makeKeyAndVisible()
 
         return true
---
+++ JumuNordost/Application/ContestsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ContestsViewController.swift
+//  JumuNordost
+//
+//  Created by Martin Richter on 13/02/16.
+//  Copyright © 2016 Martin Richter. All rights reserved.
+//
+
+import UIKit
+
+class ContestsViewController: UIViewController {
+
+    // MARK: - View Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.whiteColor()
+    }
+}
