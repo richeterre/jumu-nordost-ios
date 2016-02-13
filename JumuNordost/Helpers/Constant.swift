18864bb44c890867e50bdfd934d17a0945f5f0ac
Martin Richter <me@martinrichter.net>
2016-02-13 15:32:19 +0100

    Create constant helper with base URL

---
+++ JumuNordost/Helpers/Constant.swift
@@ -0,0 +1,13 @@
+//
+//  Constant.swift
+//  JumuNordost
+//
+//  Created by Martin Richter on 13/02/16.
+//  Copyright © 2016 Martin Richter. All rights reserved.
+//
+
+import Foundation
+
+struct Constant {
+    static let baseURL = NSURL(string: "http://localhost:5000/api/v1/")!
+}
