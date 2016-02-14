ca7967175a36f865181504697ec568b4324bb30e
Martin Richter <me@martinrichter.net>
2016-02-14 16:42:59 +0100

    Parse time zone and start date in Contest model

--- JumuNordost/Models/Contest.swift
+++ JumuNordost/Models/Contest.swift
@@ -11,20 +11,40 @@ import Argo
 struct Contest {
     let id: String
     let name: String
+    let timeZone: NSTimeZone
+    let startDate: NSDate
     let venues: [Venue]
 }
 
 // MARK: - Decodable
 
 extension Contest: Decodable {
-    static func create(id: String)(name: String)(venues: [Venue]) -> Contest {
-        return Contest(id: id, name: name, venues: venues)
+    static func create(id: String)(name: String)(timeZone: NSTimeZone)(startDate: NSDate)(venues: [Venue]) -> Contest {
+        return Contest(id: id, name: name, timeZone: timeZone, startDate: startDate, venues: venues)
     }
 
     static func decode(json: JSON) -> Decoded<Contest> {
+        let toTimeZone: String -> Decoded<NSTimeZone> = {
+            .fromOptional(NSTimeZone(name: $0))
+        }
+
+        let jsonDateFormatter: NSDateFormatter = {
+            let dateFormatter = NSDateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+            dateFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+            dateFormatter.timeZone = NSTimeZone(forSecondsFromGMT: 0)
+            return dateFormatter
+        }()
+
+        let toNSDate: String -> Decoded<NSDate> = {
+            .fromOptional(jsonDateFormatter.dateFromString($0))
+        }
+
         return Contest.create
             <^> json <| "id"
             <*> json <| "name"
+            <*> (json <| "timeZone" >>- toTimeZone)
+            <*> (json <| "startDate" >>- toNSDate)
             <*> json <|| "venues"
     }
 }
