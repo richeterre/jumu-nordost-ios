8215dd9b9eaa647a07b1c74464ca778ea2b8568e
Martin Richter <me@martinrichter.net>
2016-02-22 17:18:04 +0100

    Add Lato font alongside a font helper

---
+++ JumuNordost/Helpers/Font.swift
@@ -0,0 +1,48 @@
+//
+//  Font.swift
+//  JumuNordost
+//
+//  Created by Martin Richter on 22/02/16.
+//  Copyright © 2016 Martin Richter. All rights reserved.
+//
+
+import UIKit
+
+enum FontSize: CGFloat {
+    case Medium = 16
+}
+
+enum FontStyle {
+    case Normal
+    case Italic
+}
+
+enum FontWeight {
+    case Light
+    case Regular
+    case Bold
+}
+
+struct Font {
+    static func fontWithWeight(weight: FontWeight, style: FontStyle, size: FontSize) -> UIFont {
+        let fontName = fontNameForWeight(weight, style: style)
+        return UIFont(name: fontName, size: size.rawValue)!
+    }
+
+    private static func fontNameForWeight(weight: FontWeight, style: FontStyle) -> String {
+        switch (weight, style) {
+        case (.Light, .Normal):
+            return "Lato-Light"
+        case (.Light, .Italic):
+            return "Lato-LightItalic"
+        case (.Regular, .Normal):
+            return "Lato-Regular"
+        case (.Regular, .Italic):
+            return "Lato-Italic"
+        case (.Bold, .Normal):
+            return "Lato-Bold"
+        case (.Bold, .Italic):
+            return "Lato-BoldItalic"
+        }
+    }
+}
