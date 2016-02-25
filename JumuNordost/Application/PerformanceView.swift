7c1629deba0c2acc3b62e70967a9a05f8aa59cc8
Martin Richter <me@martinrichter.net>
2016-02-26 00:27:05 +0100

    Add icons for stage time and venue to performance details

--- JumuNordost/Application/PerformanceView.swift
+++ JumuNordost/Application/PerformanceView.swift
@@ -32,7 +32,9 @@ class PerformanceView: UIView {
 
     private let categoryLabel = Label(fontWeight: .Bold, fontStyle: .Normal, fontSize: .Large)
     private let ageGroupLabel = Label(fontWeight: .Regular, fontStyle: .Normal, fontSize: .Large)
+    private let stageTimeIcon = UIImageView(image: UIImage(named: "IconDate"))
     private let stageTimeLabel = Label(fontWeight: .Regular, fontStyle: .Normal, fontSize: .Medium)
+    private let venueIcon = UIImageView(image: UIImage(named: "IconLocation"))
     private let venueLabel = Label(fontWeight: .Regular, fontStyle: .Normal, fontSize: .Medium)
 
     // MARK: - Lifecycle
@@ -42,7 +44,9 @@ class PerformanceView: UIView {
 
         addSubview(categoryLabel)
         addSubview(ageGroupLabel)
+        addSubview(stageTimeIcon)
         addSubview(stageTimeLabel)
+        addSubview(venueIcon)
         addSubview(venueLabel)
 
         makeConstraints()
@@ -55,20 +59,35 @@ class PerformanceView: UIView {
     // MARK: - Layout
 
     private func makeConstraints() {
-        constrain(self, categoryLabel, ageGroupLabel, stageTimeLabel, venueLabel) { superview, categoryLabel, ageGroupLabel, stageTimeLabel, venueLabel in
+        constrain(categoryLabel, self) { categoryLabel, superview in
             categoryLabel.top == superview.top
             categoryLabel.left == superview.left
             categoryLabel.right <= superview.right
+        }
 
+        constrain(ageGroupLabel, categoryLabel, self) { ageGroupLabel, categoryLabel, superview in
             ageGroupLabel.top == categoryLabel.bottom
             ageGroupLabel.right <= superview.right
+        }
 
+        constrain(stageTimeIcon, stageTimeLabel, ageGroupLabel) { stageTimeIcon, stageTimeLabel, ageGroupLabel in
             stageTimeLabel.top == ageGroupLabel.bottom + 8
+            stageTimeLabel.left == stageTimeIcon.right + 8
+            align(centerY: stageTimeIcon, stageTimeLabel)
+        }
 
-            venueLabel.top == stageTimeLabel.bottom
+        constrain(venueIcon, venueLabel, stageTimeLabel) { venueIcon, venueLabel, stageTimeLabel in
+            venueLabel.top == stageTimeLabel.bottom + 4
+            venueLabel.left == venueIcon.right + 8
+            align(centerY: venueIcon, venueLabel)
+        }
+
+        constrain(venueLabel, self) { venueLabel, superview in
             venueLabel.bottom == superview.bottom
+        }
 
-            align(left: categoryLabel, ageGroupLabel, stageTimeLabel, venueLabel)
+        constrain(categoryLabel, ageGroupLabel, stageTimeIcon, venueIcon) { categoryLabel, ageGroupLabel, stageTimeIcon, venueIcon in
+            align(left: categoryLabel, ageGroupLabel, stageTimeIcon, venueIcon)
         }
     }
 }
